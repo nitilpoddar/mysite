1f439f90c36e667b194cc28de44488a59f099de5
Nitil Poddar <npoddar.zjef@gmail.com>
2024-05-27 17:22:49 +0530

    added student app for testing and practicing

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'student.apps.StudentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
